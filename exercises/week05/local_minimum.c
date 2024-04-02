d20a0714f323b1e7b49696ef5dfa97c3c1373ccd
meshagwe <meshagwe@gmail.com>
2024-04-02 14:49:45 +0200

    Count of local minimum points (version EOF)

---
+++ exercises/week05/local_minimum.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int count_of_local_minimums(int *numbers, int length) {
+
+    int count = 0;
+
+    for(int i = 1; i < length - 1; i++)
+        if( (numbers[i] < numbers[i + 1]) && (numbers[i] < numbers[i - 1]) )
+            count++;
+
+    return count;
+
+   
+}
+
+
+
+
+    int main() {
+
+    int cases, length;
+
+    while(scanf("%d", &length)!=EOF)
+    {
+        
+        int numbers[length];
+        for(int j = 0; j < length; j++) {
+            scanf("%d", &numbers[j]);
+        }
+    
+        printf("%d\n", count_of_local_minimums(numbers, length));
+    
+    }
+
+    
+
+    return EXIT_SUCCESS;
+}
+
+
