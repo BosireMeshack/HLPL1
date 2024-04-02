e3e0cd38fb05a69b3afe55dde5f0dbc2463145c6
meshagwe <meshagwe@gmail.com>
2024-04-02 23:51:14 +0200

    Removing spaces (version EOF)

--- exercises/week06/delete_spaces.c
+++ exercises/week06/delete_spaces.c
@@ -7,32 +7,29 @@
 char *delete_spaces(char *original) {
 
     int len = strlen(original);
-    char *result = (char* )malloc((len+1) * sizeof(char));
+    char *result = (char* )malloc((len+1));
 
     int j=0;
-    for(int i=0; i<len;i++)
+    int i;
+    for( i=0; i<len;i++)
     {
-        if(strcmp(original[i], ' ')==0)
+        if(original[i]!=' ')
         {
-            result[j++] = '.';
-            
-        }
-        else{
             result[j++] = original[i];
-            
         }
-    }
+        }
     result[j]='\0';
     return result;
 }
-
 
 int main()
 {
     char line[100];
     while(gets(line)!=EOF)
     {
-        printf("%s", delete_spaces(line));
+        char *space_free = delete_spaces(line);
+        printf("%s", space_free );
+        free(space_free);
     }
 
     return EXIT_SUCCESS;
